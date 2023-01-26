cda77fcc88dd467d31ee1752d9654d31a0327a95
Igor W. Lins <igorlins1234@gmail.com>
2023-01-26 17:20:55 -0300

    set player spawn and animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using EBAC.Core.Singleton;
+using DG.Tweening;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Header("Player")]
+    public GameObject playerPrefab;
 
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform StartPoint;
+
+    private GameObject _currentPlayer;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = StartPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From();
+    }
 }
