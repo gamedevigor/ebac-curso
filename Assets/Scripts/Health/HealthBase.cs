ab704400c3be220172a9c98aa0dbea42e0272e44
Igor W. Lins <igorlins1234@gmail.com>
2023-01-26 23:17:12 -0300

    add damage animation

--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -1,11 +1,19 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthBase : MonoBehaviour
 {
+    [Header("Life Setup")]
     public int startLife = 10;
     public bool destroyOnKill = false;
+    public float delayToDestroy = 0f;
+
+    [Header("Damage Animation")]
+    public float damageIndicationScale = 0.4f;
+    public float animationDuration = .5f;
+    public Ease ease = Ease.OutBack;
 
     private int _currentLife;
     private bool _isAlive = true;
@@ -26,6 +34,8 @@ public class HealthBase : MonoBehaviour
 
         _currentLife -= damage;
 
+        gameObject.transform.DOScaleY(-damageIndicationScale, animationDuration).SetLoops(3, LoopType.Yoyo).SetEase(ease);
+
         if (_currentLife<= 0)
         {
             Kill();
@@ -38,7 +48,7 @@ public class HealthBase : MonoBehaviour
         _isAlive = false;
         if (destroyOnKill)
         {
-            Destroy(gameObject);
+            Destroy(gameObject, delayToDestroy);
         }
     }
 }
